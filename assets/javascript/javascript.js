7691d3fb43320f5de83e73c20eebaab07af44842
JWsoundsystems <JWsoundsystems@gmail.com>
2019-10-31 00:09:24 -0700

    added click handler

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,20 +1,12 @@
-
-// var input = $("#searchtext")onclick.;
-
-
-var gifs = ["dog", "cat", "bear", "tiger"]
-
-
 var api = "http://api.giphy.com/v1/gifs/search";
 var api_key = "&api_key=z2lOmdbTNNGpdkCWgU38kgEtCOuC0eZl&limit=10";
-var query = `'?q=' + '$("#searchtext").val()'`;
-
-
+var query = "?q=";
+var keyword = "";
+document.getElementById("searchtext").value = keyword;
 
 function getData() {
     
-    
-    var xhr = $.get(api + query + api_key );
+    var xhr = $.get(api + query + keyword + api_key);
     
     xhr.done(function (response) {
         console.log("success got data", response);
@@ -26,8 +18,22 @@ function getData() {
         }
     });
     
-    
 }
+
+function clickHandler() {
+    $("#searchgifs").click(function() {
+        console.log($("#searchtext").val());
+        $("#searchtext").append(keyword);
+        getData();
+    })
+}
+clickHandler();
+
+
+
+
+
+
 
 
 
