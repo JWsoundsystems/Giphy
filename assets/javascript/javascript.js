b2beda2ac5ec873874dd496584b96dbd876be692
JWsoundsystems <JWsoundsystems@gmail.com>
2019-10-31 19:00:46 -0700

    fixed input search results

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,12 @@
 var api = "http://api.giphy.com/v1/gifs/search";
 var api_key = "&api_key=z2lOmdbTNNGpdkCWgU38kgEtCOuC0eZl&limit=10";
 var query = "?q=";
-var keyword = "";
-document.getElementById("searchtext").value = keyword;
+
 
 function getData() {
-    
-    var xhr = $.get(api + query + keyword + api_key);
+    $('.inner').empty()
+    let test = $("#searchtext").val();
+    var xhr = $.get(api + query + test + api_key);
     
     xhr.done(function (response) {
         console.log("success got data", response);
@@ -17,20 +17,9 @@ function getData() {
             $(".inner").append('<img src=' + jiff[i].images.original.url + '>')
         }
     });
-    
 }
 
-function clickHandler() {
-    $("#searchgifs").click(function() {
-        console.log($("#searchtext").val());
-        $("#searchtext").append(keyword);
-        getData();
-    })
-}
-clickHandler();
-
-
-
+$(document).on('click', '#searchgifs', getData)
 
 
 
