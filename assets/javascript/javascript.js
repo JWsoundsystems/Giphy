e23f58d5ae388e1bd9eb6c756c31375d279ee32e
JWsoundsystems <JWsoundsystems@gmail.com>
2019-11-06 22:17:54 -0800

    updated functionality

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,90 +1,51 @@
 var api = "http://api.giphy.com/v1/gifs/search";
 var api_key = "&api_key=z2lOmdbTNNGpdkCWgU38kgEtCOuC0eZl&limit=10";
 var query = "?q=";
-let button = $("#groups").val();
 
+let array = ['cat', 'dog', 'bear']
 
-
-function getData() {
-    $('.inner').empty()
-    let test = $("#searchtext").val();
-    var xhr = $.get(api + query + test + api_key);
-    console.log(test);
-
-    
-    xhr.done(function (response) {
-        console.log("success got data", response);
-        
-        var jiff = response.data
-        
-        
-        for (i in jiff) {
-            $(".inner").append('<img src=' + jiff[i].images.original.url + '>')
-           
-        }
-        $("#groups").append(`<button type="button">${test}</button>`);
-    });
+function createButton() {
+  let text = $("#searchtext").val();
+  if (text.length > 1) array.push(text)
+  let html = ``;
+  for (let i = 0; i < array.length; i++) {
+    html += `<button class="button">${array[i]}</button>`
+  }
+  $("#groups").html(html);
 }
 
+createButton()
 
-$(".inner").on("click", function() {
-  
-  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-  var jiff = $(this).attr("data-state");
-  console.log($(this).attr("data-state"));
-  
-  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-  // Then, set the image's data-state to animate
-  // Else set src to the data-still value
-  if (jiff === "still") {
-    $(this).attr("src=", $(this).attr("data-animate"));
-    $(this).attr("data-state", "animate");
+function buttonClick() {
+  $(".inner").empty();
+  // console.log("click", $(this).text())
+  let text = $(this).text();
+  var xhr = $.get(api + query + text + api_key).done(function (response) {
+    // console.log("success got data", response);
+
+    var jiff = response.data
+
+
+    for (i in jiff) {
+      $(".inner").append(`<img data-animated="${jiff[i].images.original.url}" data-still="${jiff[i].images.original_still.url}" class="animated" src="${jiff[i].images.original.url}">`)
+
+    }
+  })
+};
+
+function imageChange() {
+  console.log($(this).attr('data-still'))
+  let oldClassName = $(this).attr("class")
+  let newClassName = ''
+  if (oldClassName === "animated") {
+    newClassName = "still"
   } else {
-    $(this).attr("src", $(this).attr("data-still"));
-    $(this).attr("data-state", "still"); 
+    newClassName = "animated"
   }
+  $(this).attr("class", newClassName)
+  $(this).attr("src", $(this).attr(`data-${newClassName}`))
+}
 
-});
-
-$("#groups").on("click", function() {
-  $('.inner').empty()
-    let punch = $("#groups").text();
-    console.log(punch)
-
-
-    var xhr = $.get(api + query + punch + api_key);
-
-    
-    xhr.done(function (response) {
-        console.log("success got data", response);
-        
-        var jiff = response.data
-        
-        
-        for (i in jiff) {
-            $(".inner").append('<img src=' + jiff[i].images.original.url + '>')
-           
-        }
-
-    });
-});
-
-
-
-$(document).on('click', '#searchgifs', getData)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).on('click', ".still, .animated", imageChange)
+$(document).on('click', ".button", buttonClick)
+$(document).on('click', '#searchgifs', createButton)
